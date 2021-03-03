0f1de731cdc7fe86b31e5885756518b896cc2092
LorenaFFC <fernandescunhalorena3@gmail.com>
2021-03-03 15:02:41 -0300

    Inserindo Cor no método ImprimirTabuleiro

--- Xadrez-Console/Xadrez-Console/Program.cs
+++ Xadrez-Console/Xadrez-Console/Program.cs
@@ -8,22 +8,15 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine();
-            Console.WriteLine(pos.toPosicao());
-
-
-
-            /*
+           
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
 
 
                 Tela.imprimirTabuleiro(tab);
@@ -35,7 +28,7 @@ namespace Xadrez_Console
                 Console.WriteLine(t.Message);
             }
 
-            */
+            
 
         }
     }
